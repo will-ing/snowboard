eeb9d923538a71e3eeb12764a21a4d5dc0ae6d81
will-ing <willkoger@gmail.com>
2020-01-23 13:28:18 -0800

    added functions

--- prompt.js
+++ prompt.js
@@ -1,23 +1,28 @@
 
-   function txt(){
-    var mywindow = window.open("", "MsgWindow")
-    mywindow.document.write("<h3>That is a good one! check these out below!<h3>").submit();
+//    function txt(){
+//     var mywindow = window.open("", "MsgWindow")
+//     mywindow.document.write("<h3>That is a good one! check these out below!<h3>").submit();
+// }
+
+function favoriteSb(){
+    var snowboard = prompt('what is your favorite snowboard?');
+    alert(snowboard + ' is a great snowboard!');
+    return ('<strong style="color: red;">' + snowboard + "'s are awesome!</strong>");
+}
+function favColor(){
+    var color = prompt(" what is your favorite color?");
+    document.body.style.backgroundColor = color;
+    return (color + " is pretty.");
 }
 
-var snowboard = prompt('what is your favorite snowboard?');
-
-alert(snowboard + ' is a great snowboard!');
-
-var color = prompt(" what is your favorite color?");
-
-document.body.style.backgroundColor = color;
-
-confirm("DO you like snowboarding?")
-
-if( confirm === true){
-    alert("Thats awesome")
-}else{ 
-    alert("that is too bad.")
+function question(){
+    confirm("DO you like snowboarding?")
+    if( confirm === true){
+        alert("Thats awesome")
+    }else{ 
+        alert("that is too bad.")
 }
-
+}
 // document.write("")
+
+// window.location.href = "url
