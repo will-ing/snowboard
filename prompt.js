57c6baf36f67bacca77043cb30e4e36a4f15592c
will-ing <willkoger@gmail.com>
2020-01-23 21:41:59 -0800

    added loops

--- prompt.js
+++ prompt.js
@@ -1,29 +1,40 @@
 
-//    function txt(){
-//     var mywindow = window.open("", "MsgWindow")
-//     mywindow.document.write("<h3>That is a good one! check these out below!<h3>").submit();
+// function favoriteSb(){
+//     var snowboard = prompt('what is your favorite snowboard?');
+    
+//     if (snowboard === ("arbor" || "burton")){
+//     alert(snowboard + ' is a great snowboard!');
+//     return ('<strong style="color: red;">' + snowboard + "'s are awesome!</strong>");
+//     } else {alert(snowboard + '!! Try arbor or burton!');
+//     return (favoriteSb());
+//     }
 // }
+
 
 function favoriteSb(){
     var snowboard = prompt('what is your favorite snowboard?');
     
-    if (snowboard === ("arbor" || "burton")){
-    alert(snowboard + ' is a great snowboard!');
-    return ('<strong style="color: red;">' + snowboard + "'s are awesome!</strong>");
-    } else {alert(snowboard + '!! Try arbor or burton!');
-    return (favoriteSb());
+    while (snowboard == "arbor" || snowboard === "burton"){
+        alert(snowboard + ' is a great snowboard!');
+        return ('<strong style="color: red;">' + snowboard + "'s are awesome!</strong>");
+        } favoriteSb()
     }
-}
+
+function howMany(){
+    for ( i = 0; i <= 8; i++){
+        document.write(i);
+        }
+    }
+  
+
+
+
 
 function favColor(){
     var color = prompt(" what is your favorite color?");
 
-    // if (color === ){
     document.body.style.backgroundColor = color;
     return (color + " is pretty.");
-    // } else { 
-    // return (favColor());
-    // }
 }
 
 function question(){
